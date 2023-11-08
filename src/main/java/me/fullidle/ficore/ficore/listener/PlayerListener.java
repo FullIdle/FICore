c68fea56a3c5f72eb2dd0e2b2f4bdd1ec1e83fd9
FullIdle <2675423979@qq.com>
2023-11-08 21:46:21 +0800

    修复

--- src/main/java/me/fullidle/ficore/ficore/listener/PlayerListener.java
+++ src/main/java/me/fullidle/ficore/ficore/listener/PlayerListener.java
@@ -9,33 +9,44 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.InventoryHolder;
 
+import java.util.function.Consumer;
+
 public class PlayerListener implements Listener {
     @EventHandler
     public void onInvOpen(InventoryOpenEvent e){
         InventoryHolder holder = e.getInventory().getHolder();
         if (!(holder instanceof ListenerInvHolder))return;
         ListenerInvHolder invHolder = (ListenerInvHolder) holder;
-        invHolder.getOpen().accept(e);
+        Consumer<InventoryOpenEvent> consumer = invHolder.getOpen();
+        if (consumer != null){
+            consumer.accept(e);
+        }
     }
     @EventHandler
     public void onInvClick(InventoryClickEvent e){
         InventoryHolder holder = e.getInventory().getHolder();
         if (!(holder instanceof ListenerInvHolder))return;
         ListenerInvHolder invHolder = (ListenerInvHolder) holder;
-        invHolder.getClick().accept(e);
-    }
+        Consumer<InventoryClickEvent> consumer = invHolder.getClick();
+        if (consumer != null){
+            consumer.accept(e);
+        }    }
     @EventHandler
     public void onInvDrag(InventoryDragEvent e){
         InventoryHolder holder = e.getInventory().getHolder();
         if (!(holder instanceof ListenerInvHolder))return;
         ListenerInvHolder invHolder = (ListenerInvHolder) holder;
-        invHolder.getDrag().accept(e);
-    }
+        Consumer<InventoryDragEvent> consumer = invHolder.getDrag();
+        if (consumer != null){
+            consumer.accept(e);
+        }    }
     @EventHandler
     public void onClose(InventoryCloseEvent e){
         InventoryHolder holder = e.getInventory().getHolder();
         if (!(holder instanceof ListenerInvHolder))return;
         ListenerInvHolder invHolder = (ListenerInvHolder) holder;
-        invHolder.getClose().accept(e);
-    }
+        Consumer<InventoryCloseEvent> consumer = invHolder.getClose();
+        if (consumer != null){
+            consumer.accept(e);
+        }    }
 }
