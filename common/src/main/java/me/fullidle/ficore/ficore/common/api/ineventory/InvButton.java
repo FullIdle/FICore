b3bd5bcc35a029ee01e1dca2bb5e74c71f15dead
FullIdle <2675423979@qq.com>
2025-10-28 21:26:21 +0800

    增加简单的重启配置解析，和物品工具类简单解析

---
+++ common/src/main/java/me/fullidle/ficore/ficore/common/api/ineventory/InvButton.java
@@ -0,0 +1,30 @@
+package me.fullidle.ficore.ficore.common.api.ineventory;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.val;
+import me.fullidle.ficore.ficore.common.api.util.ItemStackUtil;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+public class InvButton {
+    private String id;
+    private ItemStack displayItem;
+
+    public InvButton(
+            String id,
+            ItemStack displayItem
+    ) {
+        this.id = id;
+        this.displayItem = displayItem;
+    }
+
+    public static InvButton parse(String id,ConfigurationSection section) {
+        return new InvButton(id, ItemStackUtil.parseSimpleYaml(section));
+    }
+}
---
+++ common/src/main/java/me/fullidle/ficore/ficore/common/api/util/ItemStackUtil.java
@@ -0,0 +1,38 @@
+package me.fullidle.ficore.ficore.common.api.util;
+
+import lombok.val;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class ItemStackUtil {
+    /**
+     * 解析简单的yaml物品格式
+     */
+    public static ItemStack parseSimpleYaml(ConfigurationSection section) {
+        val materialName = Objects.requireNonNull(Objects.requireNonNull(section).getString("material"));
+        val material = Objects.requireNonNull(Material.getMaterial(materialName));
+        val itemStack = new ItemStack(material);
+        val itemMeta = itemStack.getItemMeta();
+        assert itemMeta != null;
+        itemMeta.setDisplayName(section.getString("name"));
+        itemMeta.setLore(section.getStringList("lore"));
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    /**
+     * 只读3项
+     */
+    public static ItemStack parseSimpleMap(Map<String, Object> map) {
+        val yaml = new YamlConfiguration();
+        yaml.set("material",map.get("material"));
+        yaml.set("name",map.get("name"));
+        yaml.set("lore",map.get("lore"));
+        return parseSimpleYaml(yaml);
+    }
+}
