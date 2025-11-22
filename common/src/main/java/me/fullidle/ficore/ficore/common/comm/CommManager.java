d0d9534e9293eaab6cb33f67f20cbbee9b7ce096
FullIdle <2675423979@qq.com>
2025-11-22 12:34:50 +0800

    重写通讯管理器

--- common/src/main/java/me/fullidle/ficore/ficore/common/comm/CommManager.java
+++ common/src/main/java/me/fullidle/ficore/ficore/common/comm/CommManager.java
@@ -1,62 +1,113 @@
 package me.fullidle.ficore.ficore.common.comm;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-public final class CommManager {
+public abstract class CommManager {
     private final Map<Class<? extends IMessage>, HashSet<? extends IHandler<? extends IMessage>>> MESSAGE_SUBSCRIPTIONS = new HashMap<>();
+    private final BiMap<Integer, Class<? extends IMessage>> MESSAGE_TYPES = HashBiMap.create();
 
     public <T extends IMessage, E extends IHandler<T>> HashSet<E> getHandlers(final Class<T> type) {
         return (HashSet<E>) MESSAGE_SUBSCRIPTIONS.computeIfAbsent(type, k -> new HashSet<IHandler<T>>());
     }
 
+    public void registerMessage(final Class<? extends IMessage> type, final int id) {
+        if (this.MESSAGE_TYPES.containsValue(type))
+            throw new IllegalArgumentException(type.getName() + "Message type already registered!");
+        if (this.MESSAGE_TYPES.containsKey(id)) throw new IllegalArgumentException(id + "Message id already registered!");
+        this.MESSAGE_TYPES.put(id, type);
+    }
+
+    /**
+     * 订阅信息
+     * @param clazz 信息类型
+     * @param handler 处理器
+     * @return 订阅对象
+     */
     public <T extends IMessage, E extends IHandler<T>> SubscriptionObject<T> subscribe(final Class<T> clazz, final E handler) {
+        if (!this.MESSAGE_TYPES.containsValue(clazz))
+            throw new IllegalArgumentException("Message type not registered!");
         val handlers = getHandlers(clazz);
         if (handlers.contains(handler)) throw new IllegalArgumentException("Handler already subscribed!");
         handlers.add(handler);
         return new SubscriptionObject<>(this, clazz, handler);
     }
 
+    /**
+     * 取消订阅
+     * @param subscriptionObject 订阅对象
+     */
     public <T extends IMessage> void unsubscribe(final SubscriptionObject<T> subscriptionObject) {
         getHandlers(subscriptionObject.getType()).remove(subscriptionObject.getHandler());
     }
 
-    public <T extends IMessage> void publish(final T message) {
-        getHandlers(((Class<T>) message.getClass())).forEach(handler -> handler.handle(message));
+    /**
+     * 编码
+     * @param data 编码前数据
+     * @param message 信息
+     * @return 编码后数据
+     */
+    public @NotNull ByteArrayDataOutput encode(@NotNull ByteArrayDataOutput data, IMessage message) {
+        val id = this.MESSAGE_TYPES.inverse().get(message.getClass());
+        if (id == null) throw new IllegalArgumentException("Message type not registered!");
+        data.writeInt(id);
+        message.encode(data);
+        return data;
     }
 
     /**
-     * 将该方法用在你要用来通讯得地方对接上
+     * 解码
+     * @param data 数据
+     * @return 数据信息 解码失败和不存在注册的id返回null
      */
-    public <T extends IMessage> void receive(final ByteArrayDataInput byteIn) {
+    public @Nullable IMessage decode(ByteArrayDataInput data) {
+        val id = data.readInt();
+        val clazz = this.MESSAGE_TYPES.get(id);
+        if (clazz == null) return null;
         try {
-            publish(receive(((Class<T>) Class.forName(byteIn.readUTF())).newInstance(), byteIn));
-        } catch (ClassCastException | ClassNotFoundException | InstantiationException |
-                 IllegalAccessException ignored) {
+            val message = clazz.getConstructor().newInstance();
+            message.decode(data);
+            return message;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
-    public <T extends IMessage> T receive(final T message, final ByteArrayDataInput byteIn) {
-        message.decode(byteIn);
-        return message;
+    /**
+     * 通知发布
+     * @param message 信息
+     */
+    public void publish(IMessage message) {
+        publish(encode(ByteStreams.newDataOutput(), message));
     }
 
     /**
-     * 在你可以发送通讯的地方将格式化好后的数据发出
+     * 发布
+     * @param data 数据
      */
-    public ByteArrayDataOutput formatMessage(final IMessage message) {
-        return formatMessage(ByteStreams.newDataOutput(), message);
+    public abstract void publish(ByteArrayDataOutput data);
+
+    public void receive(ByteArrayDataInput data) {
+        val decode = decode(data);
+        if (decode == null) return;
+        receive(decode);
     }
 
-    public ByteArrayDataOutput formatMessage(ByteArrayDataOutput byteOut, IMessage message) {
-        byteOut.writeUTF(message.getClass().getName());
-        message.encode(byteOut);
-        return byteOut;
+    public void receive(IMessage message) {
+        val handlers = this.MESSAGE_SUBSCRIPTIONS.get(message.getClass());
+        if (handlers == null) return;
+        for (IHandler<?> handler : handlers) ((IHandler<IMessage>) handler).handle(message);
     }
 }
